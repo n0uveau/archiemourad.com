c13b6561746df3e50dd90f2c5feeba5e7188f8f8
Archie Mourad <mouradarchie+github@gmail.com>
2026-06-07 17:32:13 +1000

    feat: add ascii art hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import { art } from "@/lib/ascii";
+
 export default function Home() {
   return (
-    <main className="flex flex-1 items-center justify-center">
-      <div className="px-4 py-2 border border-foreground">archiemourad.com</div>
+    <main className="flex flex-col flex-1 lg:flex-row gap-8 lg:gap-0 pt-8 lg:pt-0">
+      <div className="flex lg:flex-1 items-center justify-center px-8">
+        <pre className="text-xs hidden lg:block">{art.full}</pre>
+        <pre className="text-xs lg:hidden">{art.compact}</pre>
+      </div>
+
+      <div className="flex lg:flex-1 items-center justify-center">
+        <div className="px-4 py-1 border border-foreground">
+          archiemourad.com
+        </div>
+      </div>
     </main>
   );
 }
